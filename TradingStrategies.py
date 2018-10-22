cb5ed2557a9fe7b8101c8a2544940c27f6085c2d
randysuen1991 <randysuen1991@gmail.com>
2018-10-22 16:28:59 +0800

    new

--- TradingStrategies.py
+++ TradingStrategies.py
@@ -1,31 +1,38 @@
 import numpy as np
 from DataAnalysis import DataAnalysis as DA
-
+import matplotlib.pyplot as plt
 
 class PairTrading:
     def __init__(self, regressor):
         self.regressor = regressor()
         self.residual = None
+        self.result = None
 
     def fit(self, series1, series2):
-        self.regressor.fit(x_train=series1, y_train=series2)
-        prediction = self.regressor.predict(series1)
-        self.residual = (series2-prediction).ravel()
-        result = DA.TimeSeriesAnalysis.adfuller(self.residual)
-        return result
+        self.regressor.fit(x_train=series1.reshape(-1, 1), y_train=series2.reshape(-1, 1))
+        prediction = self.regressor.predict(series1.reshape(-1, 1))
+        self.residual = (prediction - series2.reshape(-1, 1)).ravel()
+        self.result = DA.TimeSeriesAnalysis.adfuller(self.residual)
+        return self.result
 
+    # return the days in which we should go in and we should go out.
     def simulate(self, in_threshold, out_threshold, series=None):
         if series is None:
             series = self.residual
-
+        plt.plot(series)
+        plt.show()
         in_set = set()
         for _in in in_threshold:
             in_days = np.where(np.abs(series) > _in)[0]
-            in_set.add(in_days)
+            print(in_days)
+            for day in in_days:
+                in_set.add(day)
 
         out_set = set()
         for _out in out_threshold:
             out_days = np.where(np.abs(series) < _out)[0]
-            out_set.add(out_days)
+            # print(out_days)
+            for day in out_days:
+                out_set.add(day)
 
         return in_set, out_set
