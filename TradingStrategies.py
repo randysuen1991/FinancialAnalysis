dd7b6f7020dedb660e3fa3cced1cae3218a9991b
randysuen1991 <randysuen1991@gmail.com>
2018-10-26 12:03:06 +0800

    new

--- TradingStrategies.py
+++ TradingStrategies.py
@@ -9,68 +9,86 @@ class PairTrading:
         self.residual = None
         self.result = None
         self.maximum_position = 1
+        self.dates = None
 
-    def fit(self, series1, series2):
+    def fit(self, series1, series2, dates):
+        self.dates = dates
         self.regressor.fit(x_train=series1.reshape(-1, 1), y_train=series2.reshape(-1, 1))
         prediction = self.regressor.predict(series1.reshape(-1, 1))
         self.residual = (prediction - series2.reshape(-1, 1)).ravel()
         self.result = DA.TimeSeriesAnalysis.adfuller(self.residual)
         return self.result
 
+    def transform(self, series1, series2, dates):
+        self.dates = dates
+        prediction = self.regressor.predict(series1.reshape(-1, 1))
+        residual = (prediction - series2.reshape(-1, 1)).ravel()
+        return residual, DA.TimeSeriesAnalysis.adfuller(residual)
+    
     # return the days in which we should go in and we should go out.
     def simulate(self, in_threshold, out_threshold, series=None, plot=False):
         if series is None:
             series = self.residual
 
+        std = np.std(series)
+        mean = np.mean(series)
+
+        series_std = series - mean
+        series_std /= std
+
+        days = list()
+        actions = list()
+
         last_price = None
         position = 0
         earn = 0
-        for num in series:
+        earns = list()
+        residuals = list()
+        for i, num in enumerate(series_std):
             if abs(num) > in_threshold and abs(position) < self.maximum_position:
                 if position != 0:
                     if num > in_threshold and position < 0:
                         earn += (num-last_price)
                         position = 1
                         last_price = num
-                        print('offset and long', num, earn)
+                        actions.append('o&s')
+                        days.append(i)
+                        earns.append(earn)
+                        residuals.append(series[i])
                     elif num < in_threshold and position > 0:
                         earn += (last_price-num)
                         position = -1
                         last_price = num
-                        print('offset and short', num, earn)
+                        actions.append('o&l')
+                        days.append(i)
+                        earns.append(earn)
+                        residuals.append(series[i])
                 else:
+                    days.append(i)
                     if num > 0:
                         position += 1
                         last_price = num
-                        print('long', num)
+                        actions.append('s')
+                        residuals.append(series[i])
+                        earns.append('')
                     else:
                         position -= 1
                         last_price = num
-                        print('short', num, earn)
+                        actions.append('l')
+                        earns.append('')
+                        residuals.append(series[i])
             elif abs(num) < out_threshold and position != 0:
                 position = 0
                 earn += abs(last_price - num)
+                days.append(i)
+                actions.append('o')
+                residuals.append(series[i])
                 last_price = 0
-                print('offset', num, earn)
+                earns.append(earn)
         if plot:
             plt.plot(series)
             plt.show()
 
-        return position, earn
+        return position, earns, list(self.dates[days]), actions, residuals
 
-        # in_set = set()
-        # for _in in in_threshold:
-        #     in_days = np.where(np.abs(series) > _in)[0]
-        #     print(in_days)
-        #     for day in in_days:
-        #         in_set.add(day)
-        #
-        # out_set = set()
-        # for _out in out_threshold:
-        #     out_days = np.where(np.abs(series) < _out)[0]
-        #     # print(out_days)
-        #     for day in out_days:
-        #         out_set.add(day)
-        #
-        # return in_set, out_set
 
