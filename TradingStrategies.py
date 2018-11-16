0525dd1a4d725455afacb03b33c55acf51afed95
randysuen1991 <randysuen1991@gmail.com>
2018-11-16 10:13:33 +0800

    new

--- TradingStrategies.py
+++ TradingStrategies.py
@@ -2,6 +2,13 @@ import numpy as np
 from DataAnalysis import DataAnalysis as DA
 import matplotlib.pyplot as plt
 from scipy import stats
+import os
+import sys
+
+def save_file(df, filename):
+    this_dir, _ = os.path.split(__file__)
+    data_path = os.path.join(this_dir, 'data', filename)
+    df.to_excel(data_path)
 
 
 class PairTrading:
@@ -12,6 +19,8 @@ class PairTrading:
         self.result = None
         self.maximum_position = 1
         self.dates = None
+        self.std = None
+        self.mean = None
 
     def fit(self, series1, series2, dates):
         self.dates = dates
@@ -19,6 +28,8 @@ class PairTrading:
         prediction = self.regressor.predict(series1.reshape(-1, 1))
         self.residual = (prediction - series2.reshape(-1, 1)).ravel()
         self.result = DA.TimeSeriesAnalysis.adfuller(self.residual)
+        self.std = np.std(self.residual)
+        self.mean = np.mean(self.residual)
         return self.result
 
     def transform(self, series1, series2, dates):
@@ -32,11 +43,10 @@ class PairTrading:
         if series is None:
             series = self.residual
 
-        std = np.std(series)
-        mean = np.mean(series)
+        series_std = series - self.mean
+        series_std /= self.std
 
-        series_std = series - mean
-        series_std /= std
+        series_std = np.around(series_std, 5)
 
         days = list()
         actions = list()
@@ -91,40 +101,51 @@ class PairTrading:
             plt.plot(series)
             plt.show()
 
-        return position, earns, list(self.dates[days]), actions, residuals
+        return position, earns, list(self.dates[days]), actions, residuals, series_std[days]
 
 
 class CumulativeVolumeTrading:
 
-    def __init__(self, stocks, mean_dict, std_dict, start_time, end_time):
+    def __init__(self, stocks, mean_std_dict, start_time, end_time):
         self.stocks = stocks
-        self.mean_dict = mean_dict
-        self.std_dict = std_dict
+        self.mean_std_dict = mean_std_dict
         self.start_time = start_time
         self.end_time = end_time
         str_name = 'vol_diff_'
         for time in self.start_time + self.end_time:
             str_name += time
         self.str_name = str_name
+        self.diff_series = list()
 
     def trade(self, df, n_std, threshold):
         long_count = 0
         short_count = 0
-
+        long_list = list()
+        short_list = list()
         for stock in self.stocks:
-            vol_diff = df.loc[stock, self.str_name]
+            vol_diff = (df.loc[stock, self.str_name] - self.mean_std_dict[int(stock)][0]) / \
+                        self.mean_std_dict[int(stock)][1]
             if vol_diff > n_std:
                 long_count += 1
+                long_list.append((stock, round(vol_diff, 2)))
             elif vol_diff < -n_std:
                 short_count += 1
+                short_list.append((stock, round(vol_diff, 2)))
 
-        print('Number of long:', long_count)
-        print('Number of short:', short_count)
+        print('Number of longs:', long_count)
+        print('Stocks of the longs:', long_list)
+        print('Number of shorts:', short_count)
+        print('Stocks of the shorts:', short_list)
+        print('Differential:', long_count - short_count)
+
+        self.diff_series.append(long_count - short_count)
 
         if long_count - short_count >= threshold:
-            print('Long!')
+            # print('Long!')
+            pass
         elif long_count - short_count <= -threshold:
-            print('Short!')
+            # print('Short!')
+            pass
 
 
 class AdditiveTrading:
